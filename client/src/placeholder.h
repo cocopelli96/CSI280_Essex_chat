85a24b3c323ec9ac2a55cafbbaa4c78688bbc0af
Travis Spinelli <travisspinelli@stu-99-147-171-184.champlain.edu>
2017-03-23 21:43:19 -0400

    fixing bugs in code

--- client/src/placeholder.h
+++ client/src/placeholder.h
@@ -4,10 +4,10 @@
 #include "message.h"
 #include "message_handler.h"
 
-char* getUsername(char* local_username, int user_id);
+char* getUsername(const char* local_username, int user_id);
 
 int getColorID(int user_id);
 
-void fakeServer(Message* message, MessageHandler* handler);
+void fakeServer(const Message* message, MessageHandler* handler);
 
 #endif
