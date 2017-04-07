60792077c0e2a742c6b26c5405e183cf5ca2a607
Matthew James Harrison <mharrison@fedora.black>
2017-04-06 23:28:07 -0400

    Purged hard tabs from client source code

--- client/src/placeholder.cpp
+++ client/src/placeholder.cpp
@@ -7,26 +7,26 @@
 
 char* getUsername(const char* local_username, int user_id)
 {
-	static char buf[50];
+    static char buf[50];
     
-	if(user_id)
-	{
-		snprintf(buf, 50, "User%d", user_id);
-	}
-	else
-	{
-		snprintf(buf, 50, "%s", local_username);
-	}
+    if(user_id)
+    {
+        snprintf(buf, 50, "User%d", user_id);
+    }
+    else
+    {
+        snprintf(buf, 50, "%s", local_username);
+    }
     
     return buf;
 }
 
 int getColorID(int user_id)
 {
-	return user_id ? 2 : 3;
+    return user_id ? 2 : 3;
 }
 
 void fakeServer(const Message* message, MessageHandler* handler)
 {
-	handler->accept(message);
+    handler->accept(message);
 }
